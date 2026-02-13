a7ad618eaa7c620145b77d753c6af5e9afa36197
Cengiz Özdemir <63748401+CengizOzdemir0@users.noreply.github.com>
2026-02-13 11:13:12 +0300

    Duplicate bean hatası ve JAR yolu düzeltildi

--- src/main/java/com/cengiz/crm/security/LoginSuccessHandler.java
+++ src/main/java/com/cengiz/crm/security/LoginSuccessHandler.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +17,6 @@ import java.io.IOException;
  * 
  * @author Cengiz
  */
-@Component
 @RequiredArgsConstructor
 @Slf4j
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
